835b19ec0f2fa0850809cc0869522161f7043e5f
lvlv6994 <lvlv6994@gmail.com>
2024-06-13 23:40:37 +0800

    helloworld

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,9 @@
+
+
+const helloworld_var = "hello world"
 function helloworld(){
+    console.log(helloworld_var)
     console.log("hello world")
 }
+
+helloworld()
--- helloworld.md
+++ helloworld.md
@@ -1,4 +1,7 @@
 ## hello world
+    
+    人能走的事情，和我能做的事情是什么？艺术是不是最后的方向呢？
+
 
     ```bash
     echo "hello world"
